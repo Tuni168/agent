ec249fd4ff3720c92cc114959d688c3d5e0d2471
Futhi <Futhi@5CG9422Y1H>
2024-11-04 11:32:39 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace agent.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ details/Details.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Azure.Documents;
+
+namespace agent.details
+{
+    public class Details
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+    public class Appointment
+    {
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+    }
+    public class InMemoryDatabase
+    {
+        public static List<User> Users = new List<User>();
+        public static List<Appointment> Appointments = new List<Appointment>();
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("1. Register");
+                Console.WriteLine("2. Login");
+                Console.WriteLine("3. Exit");
+                var choice = Console.ReadLine();
+
+            }
+        }
